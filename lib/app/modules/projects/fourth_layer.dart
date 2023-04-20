3bf239710226fe5fb62beae7fb89a45fddd9c2f9
SamuelXimenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2023-04-20 21:10:11 +0100

    Refactored projects data on fourth layer page

--- lib/app/modules/projects/fourth_layer.dart
+++ lib/app/modules/projects/fourth_layer.dart
@@ -15,67 +15,94 @@ class FourthLayer extends StatefulWidget {
 class _FourthLayerState extends State<FourthLayer> {
   final List<Map<String, dynamic>> projects = [
     {
-      'title': 'Projeto 1',
-      'description': 'Descrição do Projeto 1',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
+      'hasImagePath': true,
+      'cardColorImage': Colors.red,
+      'title': 'My WebPortfolio (Este Projeto)',
+      'description':
+          'Projeto desenvolvido para usar como portfolio, falar um pouco sobre mim e mostrar meus projetos feitos.',
+      'pathImage': 'assets/images/webPortfolio.png',
+      'icons': Row(
+        children: const [
+          ImageIcon(
+            AssetImage('assets/icons/flutter.png'),
+            size: 44,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          ImageIcon(
+            AssetImage('assets/icons/github.png'),
+            size: 44,
+          ),
+        ],
+      ),
+      'link': 'https://github.com/SamuelXimenes27/myPortfolio',
+      'fit': BoxFit.fill
     },
     {
-      'title': 'Projeto 2',
-      'description': 'Descrição do Projeto 2',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
+      'hasImagePath': false,
+      'cardColorImage': Colors.red,
+      'title': 'PokedexApp',
+      'description':
+          'App feito para por em prática o consumo de API, fornecendo dados de todos os pokemons e suas características.',
+      'pathImage': '',
+      'icons': Row(
+        children: const [
+          ImageIcon(
+            AssetImage('assets/icons/flutter.png'),
+            size: 44,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          ImageIcon(
+            AssetImage('assets/icons/api.png'),
+            size: 44,
+          ),
+        ],
+      ),
+      'link': 'https://github.com/SamuelXimenes27/pokedexApp',
+      'fit': BoxFit.none
     },
     {
-      'title': 'Projeto 3',
-      'description': 'Descrição do Projeto 3',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
+      'hasImagePath': true,
+      'cardColorImage': Colors.red,
+      'title': 'Lapisco',
+      'description':
+          'App simples no qual consome a RandomUser.API e mostrando os dados tratados fornecidos por ela. Feito para o processo seletivo do Lapisco, laboratório no qual estou atualmente.',
+      'pathImage': 'assets/images/lapiscoApp.png',
+      'icons': Row(
+        children: const [
+          ImageIcon(
+            AssetImage('assets/icons/flutter.png'),
+            size: 44,
+          ),
+          ImageIcon(
+            AssetImage('assets/icons/api.png'),
+            size: 44,
+          ),
+        ],
+      ),
+      'link': 'https://github.com/SamuelXimenes27/Lapisco',
+      'fit': BoxFit.contain
     },
     {
-      'title': 'Projeto 4',
-      'description': 'Descrição do Projeto 4',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
-    },
-    {
-      'title': 'Projeto 5',
-      'description': 'Descrição do Projeto 5',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
-    },
-    {
-      'title': 'Projeto 6',
-      'description': 'Descrição do Projeto 6',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
-    },
-    {
-      'title': 'Projeto 7',
-      'description': 'Descrição do Projeto 7',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
-    },
-    {
-      'title': 'Projeto 8',
-      'description': 'Descrição do Projeto 8',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
-    },
-    {
-      'title': 'Projeto 9',
-      'description': 'Descrição do Projeto 9',
-      'icon1': const Icon(Icons.code),
-      'icon2': const Icon(Icons.android),
-      'icon3': const Icon(Icons.color_lens),
+      'hasImagePath': true,
+      'cardColorImage': Colors.red,
+      'title': 'ExpenseApp',
+      'description':
+          'App feito para controle de despesas semanais com simples gráfico feito em puro Flutter.',
+      'pathImage': 'assets/images/expenseApp.png',
+      'icons': Row(
+        children: const [
+          ImageIcon(
+            AssetImage('assets/icons/flutter.png'),
+            size: 44,
+          ),
+        ],
+      ),
+      'link': 'https://github.com/SamuelXimenes27/expensesApp',
+      'fit': BoxFit.contain
     },
   ];
 
@@ -97,198 +124,36 @@ class _FourthLayerState extends State<FourthLayer> {
                 ? Wrap(
                     spacing: 8.0,
                     runSpacing: 8.0,
-                    children: [
-                      CardProject(
-                        titleProject: 'My WebPortfolio (Este Projeto)',
-                        descriptionProject:
-                            'Projeto desenvolvido para usar como portfolio, falar um pouco sobre mim e mostrar meus projetos feitos.',
-                        pathCardImage: 'assets/images/webPortfolio.png',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/github.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
+                    children: projects.map((project) {
+                      return CardProject(
+                        hasImagePath: project['hasImagePath'],
+                        cardColorImage: project['cardColorImage'],
+                        titleProject: project['title'],
+                        descriptionProject: project['description'],
+                        pathCardImage: project['pathImage'],
+                        iconsTecnologys: project['icons'],
                         onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/myPortfolio'));
+                          launchUrl(Uri.parse(project['link']));
                         },
-                      ),
-                      CardProject(
-                        hasImagePath: false,
-                        cardColorImage: Colors.red,
-                        titleProject: 'PokedexApp',
-                        descriptionProject:
-                            'App feito para por em prática o consumo de API, fornecendo dados de todos os pokemons e suas características',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/api.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/pokedexApp'));
-                        },
-                      ),
-                      CardProject(
-                        pathCardImage: 'assets/images/lapiscoApp.png',
-                        fitImage: BoxFit.contain,
-                        titleProject: 'Lapisco',
-                        descriptionProject:
-                            'App simples no qual consome a RandomUser.API e mostrando os dados tratados fornecidos por ela. Feito para o processo seletivo do Lapisco, laboratório no qual estou atualmente.',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/api.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/Lapisco'));
-                        },
-                      ),
-                      CardProject(
-                        pathCardImage: 'assets/images/expenseApp.png',
-                        fitImage: BoxFit.contain,
-                        titleProject: 'ExpenseApp',
-                        descriptionProject:
-                            'App feito para controle de despesas semanais com simples gráfico feito em puro Flutter.',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/expensesApp'));
-                        },
-                      ),
-                    ],
+                        fitImage:
+                            project['fit'] != null ? project['fit'] : null,
+                      );
+                    }).toList(),
                   )
                 : Column(
-                    children: [
-                      CardProject(
-                        titleProject: 'My WebPortfolio (Este Projeto)',
-                        descriptionProject:
-                            'Projeto desenvolvido para usar como portfolio, falar um pouco sobre mim e mostrar meus projetos feitos.',
-                        pathCardImage: 'assets/images/webPortfolio.png',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/github.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
+                    children: projects.map((project) {
+                      return CardProject(
+                        hasImagePath: project['hasImagePath'],
+                        cardColorImage: project['cardColorImage'],
+                        titleProject: project['title'],
+                        descriptionProject: project['description'],
+                        pathCardImage: project['pathImage'],
+                        iconsTecnologys: project['icons'],
                         onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/myPortfolio'));
+                          launchUrl(Uri.parse(project['link']));
                         },
-                      ),
-                      CardProject(
-                        hasImagePath: false,
-                        cardColorImage: Colors.red,
-                        titleProject: 'PokedexApp',
-                        descriptionProject:
-                            'App feito para por em prática o consumo de API, fornecendo dados de todos os pokemons e suas características',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/api.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/pokedexApp'));
-                        },
-                      ),
-                      CardProject(
-                        pathCardImage: 'assets/images/lapiscoApp.png',
-                        fitImage: BoxFit.contain,
-                        titleProject: 'Lapisco',
-                        descriptionProject:
-                            'App simples no qual consome a RandomUser.API e mostrando os dados tratados fornecidos por ela. Feito para o processo seletivo do Lapisco, laboratório no qual estou atualmente.',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                            ImageIcon(
-                              AssetImage('assets/icons/api.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/Lapisco'));
-                        },
-                      ),
-                      CardProject(
-                        pathCardImage: 'assets/images/expenseApp.png',
-                        fitImage: BoxFit.contain,
-                        titleProject: 'ExpenseApp',
-                        descriptionProject:
-                            'App feito para controle de despesas semanais com simples gráfico feito em puro Flutter.',
-                        iconsTecnologys: Row(
-                          children: const [
-                            ImageIcon(
-                              AssetImage('assets/icons/flutter.png'),
-                              size: 44,
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          launchUrl(Uri.parse(
-                              'https://github.com/SamuelXimenes27/expensesApp'));
-                        },
-                      ),
-                    ],
+                      );
+                    }).toList(),
                   ),
           ),
         ),
